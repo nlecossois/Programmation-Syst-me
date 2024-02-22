86fa2f79ade5cebc0bb5b0532ea4ce9c40d5f346
nlecossois <lecossois.nolan@gmail.com>
2024-02-22 09:34:28 +0100

    Application Mono-Instance
    - Correction d'une erreur manquante dans le fichier de lang
    - Ajout du système d'application mono-instance via mutex

--- EasySave/EasySavev2/EasySaveV2/EasySaveV2/View.xaml.cs
+++ EasySave/EasySavev2/EasySaveV2/EasySaveV2/View.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -22,8 +23,32 @@ namespace EasySaveV2
     {
         public View()
         {
-            InitializeComponent();
-            DataContext = new ViewModel();
+            //Here we carry out the control to make the application single-instance using a mutex
+            Mutex mutex = new Mutex(true, "{F48SDQF6f-sd8g-54fs-48p2-JH2IKK6A8}");
+
+
+            //If the mutex is already taken this means that another instance of the application is running on this device
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                MessageBoxResult result = MessageBox.Show("Unable to start application : The application is already open on this computer.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //Closing the application
+                if (result == MessageBoxResult.OK)
+                {
+                    Application.Current.Shutdown();
+                }
+                return;
+
+            } else
+            {
+                InitializeComponent();
+                DataContext = new ViewModel();
+            }
+
+
+            void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+            {
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
