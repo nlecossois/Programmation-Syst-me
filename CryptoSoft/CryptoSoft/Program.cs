b055e3364d2ac358f7d5417d2b70c597b051d739
nlecossois <lecossois.nolan@gmail.com>
2024-02-27 20:27:20 +0100

    Mise en palce de CryptoSoft sur Git

---
+++ CryptoSoft/CryptoSoft/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace CryptoSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Unknow File.");
+                System.Environment.Exit(0);
+            } else
+            {
+                string filePath = args[0];
+                Encrypter.CryptFile(filePath);
+            }
+        }
+    }
+
+    public class Encrypter
+    {
+        public static void CryptFile(string filePath)
+        {
+            byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+            byte[] buffer = System.IO.File.ReadAllBytes(filePath);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ key[i % key.Length]);
+            }
+
+            System.IO.File.WriteAllBytes(filePath, buffer);
+            System.Environment.Exit(0);
+        }
+    }
+}
